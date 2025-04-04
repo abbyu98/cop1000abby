e8e28bf4ed87430afc38c7021c67ebe5976c4ac2
abby urquhart <abigailurquhart1@gmail.com>
2025-04-04 12:44:49 -0400

    Assignment 2-2 Assigning Variables in Python.py

--- Assignment 2-2 Assign Variables in Python.py
+++ Assignment 2-2 Assign Variables in Python.py
@@ -4,4 +4,4 @@ myCurrentAge = 26
 currentYear = 2025  
 myNewAge = myCurrentAge + (2050 - currentYear)
 print("My Current Age is " + str(myCurrentAge))
-print("I will be " + str(myNewAge) + " in 2050.")
+print("I will be " + str(myNewAge) + " in 2050.") 
