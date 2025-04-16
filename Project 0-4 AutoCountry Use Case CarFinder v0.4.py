1d303c15eae95e202ee47b8db989ab75f55644bb
abby urquhart <abigailurquhart1@gmail.com>
2025-04-16 15:34:08 -0400

    vehicles.txt

--- Project 0-4 AutoCountry Use Case CarFinder v0.4.py
+++ Project 0-4 AutoCountry Use Case CarFinder v0.4.py
@@ -1,6 +1,21 @@
 # AutoCountry Vehicle Finder v0.1
-#Allowed Vehicles List
-AllowedVehiclesList = ['Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan', 'Rivian R1T', 'Ram 1500' ]
+#Allowed Vehicles File
+VEHICLE_FILE = "vehicles.txt"
+
+# Function to read allowed vehicles from file
+def read_vehicles():
+    try:
+        with open(VEHICLE_FILE, "r") as file:
+            vehicles = [line.strip() for line in file.readlines()]
+        return vehicles
+    except FileNotFoundError:
+        return [] # Return an empty list if the file is not found
+    
+# Function to write updated allowed vehicles to file
+def write_vehicles(vehicles):
+    with open(VEHICLE_FILE, "w") as file:
+        for vehicle in vehicles:
+            file.write(vehicle + "\n")
 
 # print show menu
 def show_menu():
@@ -14,38 +29,44 @@ def show_menu():
     print("4. REMOVE a Vehicle")  # Allow Sales Manager to remove a vehicle
     print("5. Exit") # Exit the program
 
-#print allowed vehicles
+# print allowed vehicles from file
 def print_vehicles():
-    print("\nThe AutoCountry sales manager has authorized the purchase and selling of the following vehicles:")
-    for vehicle in AllowedVehiclesList:
-        print(vehicle) # Display each vehicle in the list
+    vehicles = read_vehicles()
+    if vehicles:
+        print("\nThe AutoCountry sales manager has authorized the purchase and selling of the following vehicles:")
+        for vehicle in vehicles:
+            print(vehicle) # Display each vehicle in the list
 
-# Function to search for a vehicle in AllowedVehiclesList
+# Function to search for a vehicle in the file
 def search_vehicle():
     search = input("\nEnter the name of the vehicle you want to search for: ") # Get user input for vehicle name
-    if search in AllowedVehiclesList:  # Check if the vehicle is in the list
+    vehicles = read_vehicles()
+    if search in vehicles:  # Check if the vehicle is in the list
         print(f"\nThe vehicle '{search}' is authorized for purchase.")  # Vehicle found message
     else:
         print(f"\nThe vehicle '{search}' is NOT authorized for purchase.")  # Vehicle not found message
 
-# Function to add a new vehicle to AllowedVehiclesList
+# Function to add a new vehicle to the file
 def add_vehicle():
     new_vehicle = input("\nEnter the name of the vehicle you want to add: ")  # Get user input for new vehicle
-    if new_vehicle in AllowedVehiclesList:
+    vehicles = read_vehicles()
+    if new_vehicle in vehicles:
         print(f"\nThe vehicle '{new_vehicle}' is already authorized.")  # Inform user if vehicle already exists
     else:
-        AllowedVehiclesList.append(new_vehicle)  # Add vehicle to the list
+        vehicles.append(new_vehicle)  # Add vehicle to the list
         print(f"\nThe vehicle '{new_vehicle}' has been successfully added to the authorized list.")  # Success message
 
-# Function to remove a vehicle from AllowedVehiclesList (Only for Sales Manager)
+# Function to remove a vehicle from the file (Only for Sales Manager)
 def remove_vehicle():
     user_role = input("\nEnter your role (Sales Manager required): ")  # Get user role
     if user_role.lower() == "sales manager":
+        vehicles = read_vehicles()  # Read the current list of vehicles
         vehicle_to_remove = input("\nEnter the name of the vehicle you want to remove: ")  # Get vehicle to remove
-        if vehicle_to_remove in AllowedVehiclesList:
+        if vehicle_to_remove in vehicles:
              confirm = input(f"\nAre you sure you want to remove \"{vehicle_to_remove}\" from the Authorized Vehicles List? (yes/no): ")
              if confirm.lower() == "yes":
-                AllowedVehiclesList.remove(vehicle_to_remove)  # Remove vehicle from the list
+                vehicles.remove(vehicle_to_remove)  # Remove vehicle from the list
+                write_vehicles(vehicles)
                 print(f"\nThe vehicle '{vehicle_to_remove}' has been successfully removed from the authorized list.")  # Success message
         else:
             print(f"\nThe vehicle '{vehicle_to_remove}' is not found in the list.")  # Vehicle not in list message
