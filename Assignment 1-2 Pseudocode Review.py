4ddfdd57ae3c947d0e6ca4163602d27f6d1f507f
abby urquhart <abigailurquhart1@gmail.com>
2025-04-22 12:45:11 -0400

    Assignment 1-2 Pseudocode Review.py

--- Assignment 1-2 Pseudocode Review.py
+++ Assignment 1-2 Pseudocode Review.py
@@ -1,5 +1,9 @@
+# This program will take a value from the user and multiply it by 10
+# Enter a value from the user
 value = float(input("Enter a value: "))
 
+# Multiply the value by 10
 result= value*10
 
+# Print the result
 print("The result is: ", result)
